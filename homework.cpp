0bcd4fba670ffaa11a5e3175d86de36aced0d817
Jamlee977 <88511754+Jamlee977@users.noreply.github.com>
2022-03-26 21:53:34 +0300

    Update homework.cpp

--- homework.cpp
+++ homework.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
-#include <algorithm>
 #include <string>
 #include <cstring>
+#include <algorithm>
+
+#define INT_MIN -2147483647
+#define INT_MAX 2147483647
 
 using namespace std;
 
@@ -14,7 +17,8 @@ using namespace std;
  * @param toCopyIn An array to be copied in.
  * @param size The size of the array.
  */
-void copy(int toBeCopied[], int toCopyIn[], int size) {
+void copy(int toBeCopied[], int toCopyIn[], int size) 
+{
 
     for (int i = 0; i < size; i++)
     {
@@ -23,39 +27,41 @@ void copy(int toBeCopied[], int toCopyIn[], int size) {
 
 }
 
+
 /**
  * @brief A median function used to get the median values whether the array was even or odd.
  * 
  * @param medianArray The array that we assign in the main function.
  * @param sizeOfMedianArray The size of the array.
  */
-void median(int medianArray[], int sizeOfMedianArray) {
+void median(int medianArray[], int sizeOfMedianArray) 
+{
 
     int evenSize;
     int oddSize;
 
     if (sizeOfMedianArray % 2 == 0) {
-        sort(medianArray, medianArray + sizeOfMedianArray);
         evenSize = (sizeOfMedianArray / 2);
 
         cout << double(medianArray[evenSize - 1] + medianArray[evenSize]) / 2;
     }
     else {
-        sort(medianArray, medianArray + sizeOfMedianArray);
         oddSize = (sizeOfMedianArray / 2);
         cout << double(medianArray[oddSize]);
     }
 }
+
 
 int main() {
 
     int medianArray[] =  {100, 10, 20, 57, 2, 64, 83, 22, 71, 65, 64, 92}; 
     int size = sizeof(medianArray) / sizeof(medianArray[0]);
 
-
     int *newMedianArray = new int[size];
 
     copy(medianArray, newMedianArray, size);
+
+    sort(newMedianArray, newMedianArray + size);
 
     median(newMedianArray, size);
 
